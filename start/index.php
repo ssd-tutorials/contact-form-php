4c0c8a8ce012801793a74b54447f91d6bc02de0d
Sebastian Sulinski <Sebastian@Sebastians-Mac-Pro.local>
2015-11-17 19:15:59 +0000

    Initial commit

---
+++ start/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width" />
+    <title>Simple Contact Form with PHP</title>
+    <link rel="stylesheet" type="text/css" href="css/foundation.min.css" />
+    <link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.min.css" />
+    <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Roboto:400,700" />
+    <link rel="stylesheet" type="text/css" href="css/core.css" />
+    <script src="js/vendor/custom.modernizr.js"></script>
+</head>
+<body>
+
+
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script src="js/foundation/foundation.js"></script>
+<script src="js/foundation/foundation.forms.js"></script>
+<script src="js/core.js"></script>
+</body>
+</html>
